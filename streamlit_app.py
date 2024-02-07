7856188c73aede3d68c175957b8c086bfa2a217b
Muzayyan13 <147156326+Muzayyan13@users.noreply.github.com>
2024-02-07 17:15:59 +0530

    Update streamlit_app.py
    added two more items in menu 
    added emojis 
    and Create another header for a special menu item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('My Parents Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinich & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinich & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocadro Tost')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
